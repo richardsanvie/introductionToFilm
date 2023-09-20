37be68d1799b3fe0a3bf4463d510778dbbd688f9
Richard <richardsanvie@gmail.com>
2023-09-20 16:10:41 -0300

    introductionVue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
+const nome = "Ya"
 
+const pessoa = {
+  nome: 'Yeyeye',
+  age: 27,
+} 
+
+function darBoasVindas(nome){
+  return `Seja bem vindo ${nome}!`
+}
+
+const imageRandom = 'https://source.unsplash.com/random/'
+
+const btn = false
 </script>
 
 <template>
-  <h1>heyehey</h1>
+  <h1>{{ nome }}</h1>
+  <p>{{ pessoa.nome }}</p>
+  <p>{{ pessoa.age }}</p>
+  <p>{{ darBoasVindas('Jo')}}</p>
+  <img :src="imageRandom" alt="" style="width: 100px;">
+  <button :disabled="!btn">Submeter</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
